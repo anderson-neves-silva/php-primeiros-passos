4d246063c53623a0392d4d15af7edef9d97e303f
Anderson Neves <andersonneves_@hotmail.com>
2023-02-09 00:16:24 -0300

    trabalhando com operações matemáticas no php

---
+++ operacoes.php
@@ -0,0 +1,21 @@
+<?php
+
+$idade = 21;
+$idadeHa5Anos = $idade - 5;
+$idadeDaqui10Anos = $idade + 10;
+echo $idadeDaqui10Anos;
+echo $idadeHa5Anos;
+
+// as quatro operações básicas
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+// operador de potência, ou elevado ao cubo, isso abaixo é o mesmo que 2 * 2 * 2
+$doisAoCubo = 2 ** 3;
+echo $doisAoCubo;
+
+// resto da divisão
+$restoDaDivisao = 10 % 3;
+echo $restoDaDivisao;
