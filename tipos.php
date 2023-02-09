88f7046854014274057fb117151600e60d6f8a2d
Anderson Neves <andersonneves_@hotmail.com>
2023-02-09 15:50:23 -0300

    trabalhando com tipo de dados  no php

---
+++ tipos.php
@@ -0,0 +1,16 @@
+<?php
+
+$idade = 21;
+$salario = 1000.30;
+$divisao = 10 / 3;
+$texto = "Olá mundo";
+$verdadeiro = true;
+$falso = false;
+
+// descobrindo o tipo da variável no php
+echo gettype($idade);
+echo gettype($salario);
+echo gettype($divisao);
+echo gettype($texto);
+echo gettype($verdadeiro);
+echo gettype($falso);
