f4f8b0303d8955ad6de18c7b0569622eaf1c3278
Anderson Neves <andersonneves_@hotmail.com>
2023-02-13 16:30:16 -0300

    trabalhando com string no php

---
+++ string.php
@@ -0,0 +1,7 @@
+<?php
+
+/* concatenando usando aspas simples  e o ponto no php, mas uma forma mais fácil e suando aspas duplas 
+como segue os exemplos abaixo */
+$idade = 29;
+echo 'Olá mundo! a minha idade é ' . $idade . ' anos';
+echo "Olá mundo! a minha idade é $idade anos";
