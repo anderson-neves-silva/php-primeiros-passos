1dd848c287b96fcde472e7b4bfcdafd0f67d3ed9
Anderson Neves <andersonneves_@hotmail.com>
2023-02-14 23:36:47 -0300

    trabalhando com caracteres especiais no php, usando a forma própria do PHP de colocar quebra de linha

--- string.php
+++ string.php
@@ -4,4 +4,15 @@
 como segue os exemplos abaixo */
 $idade = 29;
 echo 'Olá mundo! a minha idade é ' . $idade . ' anos';
-echo "Olá mundo! a minha idade é $idade anos";
+
+/* caracteres especiais, para quebra de linha use o \n mas dentro de aspas duplas, usando aspas simples ele não quebra linha e 
+apenas exibe o \n */
+echo "\n";
+// caracteres especiais, para colar um tab
+echo "\t";
+
+echo "Olá mundo! a minha idade é $idade anos \n";
+
+// caracteres especiais, o php tem uma forma própria para quebra de linha
+echo "Olá" . PHP_EOL;
+echo "Mundo!";
